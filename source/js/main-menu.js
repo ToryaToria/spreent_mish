d54874bcc48e130ef0900dcf2beeed2a6d9b352b
ToryaToria <toria.torya@yandex.ru>
2026-04-13 09:05:28 +0300

    линты

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,15 +1,11 @@
 const body = document.body;
 const btnMenu = document.querySelector('.btn-menu');
 const nav = document.querySelector('.nav');
+const btnStart = document.querySelector('#js-button');
 
-// const navLinks = document.querySelectorAll('.nav__link');
-
+const navLinks = document.querySelectorAll('.nav__link');
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
-const isEnterKey = (evt) => evt.key === 'Enter';
-const isTabKey = (evt) => evt.key === 'Tab';
-
-const isFocus = (evt) => evt.contains(document.activeElement);
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -18,50 +14,34 @@ const onDocumentKeydown = (evt) => {
 };
 
 const onDocumentClick = (evt) => {
-  if (evt.target === body) {
+  if (evt.target === nav) {
     closeMenu();
   }
 };
-
-// закрываю меню, если протабала его полностью.
-// const tabbingOutNav = (e) => {
-//   if (isTabKey(e) && !isFocus(nav)) {
-//     closeMenu();
-//   }
-// };
 
 btnMenu.addEventListener('click', () => {
   document.addEventListener('keydown', onDocumentKeydown);
   body.addEventListener('click', onDocumentClick);
 
   btnMenu.classList.toggle('btn-menu--closer');
-  body.classList.toggle('overlay');
   nav.classList.toggle('header__nav--open');
-
-
-  // navLinks.forEach((elem) => {
-  //   elem.setAttribute('tabindex', '0');
-  // });
 
 });
 
 function closeMenu() {
   btnMenu.classList.remove('btn-menu--closer');
-  body.classList.remove('overlay');
-  nav.classList.remove('header__nav--open');
-
-  // navLinks.forEach((elem) => {
-  //   elem.setAttribute('tabindex', '-1');
-  // });
+  nav.classList.add('header__nav--open');
 
   document.removeEventListener('keydown', onDocumentKeydown);
   body.removeEventListener('click', onDocumentClick);
 }
 
-// navLinks.forEach((elem) => {
-//   elem.addEventListener('click', () => {
-//     closeMenu();
-//   });
-// });
+btnStart.addEventListener('click', () => {
+  closeMenu();
+});
 
-// document.addEventListener('keyup', tabbingOutNav);
+navLinks.forEach((el) => {
+  el.addEventListener('click', () => {
+    closeMenu();
+  });
+});
