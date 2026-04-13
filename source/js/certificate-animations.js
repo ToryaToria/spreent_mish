c326a7ae9fc52c2b8b71766099d2279244cf0c02
ToryaToria <toria.torya@yandex.ru>
2026-04-13 12:06:17 +0300

    дополнительные анимации

---
+++ source/js/certificate-animations.js
@@ -0,0 +1,35 @@
+const tracked = document.querySelector('.certificate');
+
+const certificateImg = document.querySelector('.certificate__card');
+const certificateButton = document.querySelector('.certificate__button');
+const certificateText = document.querySelector('.certificate__text');
+const certificateTitle = document.querySelector('.certificate__title');
+const certificateTitleAnim = document.querySelector('.certificate__title-animation');
+
+
+function observeAnimatedElem() {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        certificateImg.classList.add('animation');
+        certificateButton.classList.add('animation');
+        certificateText.classList.add('animation');
+        certificateTitle.classList.add('animation');
+        certificateTitleAnim.classList.add('animation');
+
+      } else {
+        certificateImg.classList.remove('animation');
+        certificateButton.classList.remove('animation');
+        certificateText.classList.remove('animation');
+        certificateTitle.classList.remove('animation');
+        certificateTitleAnim.classList.remove('animation');
+
+
+      }
+    })
+  });
+
+  observer.observe(tracked);
+}
+
+document.addEventListener('DOMContentLoaded', observeAnimatedElem);
--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,8 @@
 import './main-menu.js';
+import './price-animation.js';
+import './certificate-animations.js';
+import './img-rotate.js';
+
 
 // import {validatesForm} from './form-valid.js';
 // validatesForm();
