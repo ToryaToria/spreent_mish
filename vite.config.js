2a3dfcd825038bd306fd92d4d12d855b75f6a61c
ToryaToria <toria.torya@yandex.ru>
2026-04-14 11:39:01 +0300

    готово

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import VitePluginSvgSpritemap from '@spiriit/vite-plugin-svg-spritemap';
-// import { ViteMinifyPlugin } from 'vite-plugin-minify';
+import { ViteMinifyPlugin } from 'vite-plugin-minify';
 
 /** @type {import('vite').UserConfig} */
 export default {
@@ -9,8 +9,8 @@ export default {
       styles: false,
       injectSVGOnDev: true,
     }),
-    // input https://www.npmjs.com/package/html-minifier-terser options
-    // ViteMinifyPlugin({}),
+    
+    ViteMinifyPlugin({}),
     ViteImageOptimizer({
       test: /\.(jpe?g|png|svg)$/i,
       includePublic: false,
